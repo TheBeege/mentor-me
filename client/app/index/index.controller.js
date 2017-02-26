730c3264afc9539254da28566709c9216f3dd40e
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-26 00:40:58 -0800

    cleaned project

--- client/app/index/index.controller.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('mentorMeApp')
-  .controller('IndexCtrl', function ($scope, $http, $state) {
-  });
--- client/app/no-permissions/no-permissions.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// TODO: use ui.select (angular-ui/ui-select) to do better tagging stuff
-angular.module('mentorMeApp')
-  .controller('NoPermissionsCtrl', function ($scope, $http) {
-
-  });
