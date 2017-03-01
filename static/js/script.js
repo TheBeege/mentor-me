06f3587b9c631f9cb2bdf14b6c3392374ef35917
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-28 23:42:47 -0800

    Front-end example setup. Removed junk from tpl file.

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+function main() {
+    var exampleElement = document.getElementsByClassName("script-test")[0];
+    exampleElement.innerHTML = "If you can see this, it means Javascript ran properly.";
+}
